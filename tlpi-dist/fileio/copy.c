f069418e12782bce3d2497f1413b5f69a63ebcab
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-06-04 09:42:26 -0400

    Write fileio/copy.c example

--- tlpi-dist/fileio/copy.c
+++ tlpi-dist/fileio/copy.c
@@ -8,17 +8,16 @@
 * the file COPYING.gpl-v3 for details.                                    *
 \*************************************************************************/
 
-/* Listing 4-1 */
-
 /* copy.c
 
    Copy the file named argv[1] to a new file named in argv[2].
 */
-#include <sys/stat.h>
+
+#include  <sys/stat.h>
 #include <fcntl.h>
 #include "tlpi_hdr.h"
 
-#ifndef BUF_SIZE        /* Allow "cc -D" to override definition */
+#ifndef BUF_SIZE
 #define BUF_SIZE 1024
 #endif
 
@@ -34,26 +33,25 @@ main(int argc, char *argv[])
         usageErr("%s old-file new-file\n", argv[0]);
 
     /* Open input and output files */
-
     inputFd = open(argv[1], O_RDONLY);
     if (inputFd == -1)
         errExit("opening file %s", argv[1]);
 
     openFlags = O_CREAT | O_WRONLY | O_TRUNC;
     filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
-                S_IROTH | S_IWOTH;      /* rw-rw-rw- */
+                S_IROTH | S_IWOTH; /* rw-rw-rw- */
     outputFd = open(argv[2], openFlags, filePerms);
     if (outputFd == -1)
         errExit("opening file %s", argv[2]);
 
-    /* Transfer data until we encounter end of input or an error */
-
+  /* Transfer data until we encounter end of input or an error */
     while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
         if (write(outputFd, buf, numRead) != numRead)
             fatal("couldn't write whole buffer");
     if (numRead == -1)
         errExit("read");
 
+    /* Close file descriptors */
     if (close(inputFd) == -1)
         errExit("close input");
     if (close(outputFd) == -1)
