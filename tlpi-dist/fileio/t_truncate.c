58dee80e253c14a465325b9cf9d87bcb067bc9ce
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-06-08 10:46:19 -0400

    Finish chapter 5
    Do the `t_truncate.c` example.

--- tlpi-dist/fileio/t_truncate.c
+++ tlpi-dist/fileio/t_truncate.c
@@ -1,13 +1,3 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2019.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
 /* Supplementary program for Chapter 5 */
 
 /* t_truncate.c
@@ -23,7 +13,10 @@ main(int argc, char *argv[])
     if (argc != 3 || strcmp(argv[1], "--help") == 0)
         usageErr("%s file length\n", argv[0]);
 
-    if (truncate(argv[1], getLong(argv[2], GN_ANY_BASE, "length")) == -1)
+    char *pathname = argv[1];
+    int truncationPoint = getLong(argv[2], GN_ANY_BASE, "length");
+
+    if (truncate(pathname, truncationPoint) == -1)
         errExit("truncate");
 
     exit(EXIT_SUCCESS);
