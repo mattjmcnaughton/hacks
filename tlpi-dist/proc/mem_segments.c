e88a5957b56fc31ad715a23999d07ec1e945d277
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-06-10 09:06:16 -0400

    Rewrite `proc/mem_segments`

--- tlpi-dist/proc/mem_segments.c
+++ tlpi-dist/proc/mem_segments.c
@@ -1,43 +1,31 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2019.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
-/* Listing 6-1 */
-
 /* mem_segments.c
 
    A program that does nothing in particular, but the comments indicate
    which memory segments each type of variable is allocated in.
 */
-#define _BSD_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 
-char globBuf[65536];            /* Uninitialized data segment */
-int primes[] = { 2, 3, 5, 7 };  /* Initialized data segment */
+char globBuf[65536]; /* Uninitialized data segment */
+int primes[] = { 2, 3, 5, 7 }; /* Initialized data segment */
 
+// `static` indicates func is static (i.e. only visible throughout the file)
 static int
-square(int x)                   /* Allocated in frame for square() */
+square(int x) // x is allocated in stack frame for square
 {
-    int result;                 /* Allocated in frame for square() */
+    int result; // Allocated in stack frame for square
 
     result = x * x;
-    return result;              /* Return value passed via register */
+    return result; // Return value passed via register
 }
 
 static void
-doCalc(int val)                 /* Allocated in frame for doCalc() */
+doCalc(int val) // val is allocated in stack frame for doCalc
 {
     printf("The square of %d is %d\n", val, square(val));
 
     if (val < 1000) {
-        int t;                  /* Allocated in frame for doCalc() */
+        int t; // t is allocated in stack frame for doCalc
 
         t = val * val * val;
         printf("The cube of %d is %d\n", val, t);
@@ -45,13 +33,13 @@ doCalc(int val)                 /* Allocated in frame for doCalc() */
 }
 
 int
-main(int argc, char *argv[])    /* Allocated in frame for main() */
+main(int argc, char *argv[])
 {
-    static int key = 9973;      /* Initialized data segment */
-    static char mbuf[10240000]; /* Uninitialized data segment */
-    char *p;                    /* Allocated in frame for main() */
+    static int key = 9973; // key is an initialized data segment
+    static char mbuf[10240000]; // uninitialized data segment
+    char *p;
 
-    p = malloc(1024);           /* Points to memory in heap segment */
+    p = malloc(1024); // pointer to memory allocated in heap segment
 
     doCalc(key);
 
