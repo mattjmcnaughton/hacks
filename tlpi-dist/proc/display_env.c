ca1c852c915803fce4f2a74e3a092b55acb0d33e
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-06-22 10:52:50 -0400

    Rewrite proc/display_env.c

--- tlpi-dist/proc/display_env.c
+++ tlpi-dist/proc/display_env.c
@@ -1,13 +1,3 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2019.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
 /* Listing 6-3 */
 
 /* display_env.c
@@ -17,7 +7,6 @@
 #include "tlpi_hdr.h"
 
 extern char **environ;
-                /* Or define _GNU_SOURCE to get it from <unistd.h> */
 
 int
 main(int argc, char *argv[])
