14b33c1b48af7e195676601aaf5603360afa7915
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-06-22 10:59:54 -0400

    Rewrite proc/modify_env.c

--- tlpi-dist/proc/modify_env.c
+++ tlpi-dist/proc/modify_env.c
@@ -1,13 +1,3 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2019.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
-
 /* Listing 6-4 */
 
 /* modify_env.c
@@ -19,7 +9,7 @@
    Note: some UNIX implementations do not provide clearenv(), setenv(),
    and unsetenv().
 */
-#define _GNU_SOURCE     /* Get various declarations from <stdlib.h> */
+#define _GNU_SOURCE /* Get various declarations from <stdlib.h> */
 #include <stdlib.h>
 #include "tlpi_hdr.h"
 
@@ -31,25 +21,21 @@ main(int argc, char *argv[])
     int j;
     char **ep;
 
-    clearenv();         /* Erase entire environment */
+    clearenv(); // Erase entire environment
 
-    /* Add any definitions specified on command line to environment */
-
+    // Add any definitions specified on the command line.
     for (j = 1; j < argc; j++)
         if (putenv(argv[j]) != 0)
             errExit("putenv: %s", argv[j]);
 
-    /* Add a definition for GREET if one does not already exist */
-
+    // Add a definition for GREET if one doesn't already exist
     if (setenv("GREET", "Hello world", 0) == -1)
         errExit("setenv");
 
-    /* Remove any existing definition of BYE */
-
+    // Remove any existing definition of BYE
     unsetenv("BYE");
 
-    /* Display current environment */
-
+    // Display current environment
     for (ep = environ; *ep != NULL; ep++)
         puts(*ep);
 
