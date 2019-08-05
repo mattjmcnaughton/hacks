1f3f1c19357c529090b726ba92dfd31d377b1bed
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-08-05 10:47:48 -0400

    Add simple vm designed in typescript
    Showing how simple a vm can be. Tbh, not positive if the typescript
    component actually works... was just experimenting.

---
+++ writing-a-compiler-in-go/scratch/ts-vm/virtual_machine.ts
@@ -0,0 +1,58 @@
+const PUSH = "push";
+const ADD = "add";
+const MINUS = "minus";
+
+let virtualMachine = function(program) {
+  let programCounter = 0;
+  let stack = [];
+  let stackPointer = 0;
+
+  while (programCounter < program.length) {
+    // fetch
+    let currentInstruction = program[programCounter];
+
+    // decode
+    switch (currentInstruction) {
+      // execute
+      case PUSH:
+        stack[stackPointer] = program[programCounter + 1];
+        stackPointer++;
+        programCounter++;
+        break;
+
+      case ADD:
+        right = stack[stackPointer-1];
+        stackPointer--;
+        left = stack[stackPointer-1];
+        stackPointer--;
+
+        stack[stackPointer] = left + right;
+        stackPointer++;
+        break;
+
+      case MINUS:
+        right = stack[stackPointer-1];
+        stackPointer--;
+        left = stack[stackPointer-1];
+        stackPointer--;
+
+        stack[stackPointer] = left - right;
+        stackPointer++;
+        break;
+    }
+
+    programCounter++;
+  }
+
+  console.log("stacktop: ", stack[stackPointer-1]);
+}
+
+let program = [
+  PUSH, 3,
+  PUSH, 4
+  ADD,
+  PUSH, 5,
+  MINUS
+];
+
+virtualMachine(program);
