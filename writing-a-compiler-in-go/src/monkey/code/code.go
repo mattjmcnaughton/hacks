800c8d4b840fcad22dcbb0446ae80a72e28b37e0
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-08-05 21:59:22 -0400

    [WIP] Chapter 2
    Implemented `String` and `ReadOperands` methods for Instructions.
    Returning to attempting to write the compiler.

--- writing-a-compiler-in-go/src/monkey/code/code.go
+++ writing-a-compiler-in-go/src/monkey/code/code.go
@@ -1,16 +1,55 @@
 package code
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
 
 type Instructions []byte
 
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		operands, read := ReadOperands(def, ins[i+1:])
+
+		// i is the location of the current byte we are reading
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+
+		i += read + 1
+	}
+
+	return out.String()
+}
+
+func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
+	operandCount := len(def.OperandWidths)
+
+	if len(operands) != operandCount {
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n", len(operands), operandCount)
+	}
+
+	switch operandCount {
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
+}
+
 type Opcode byte
 
 const (
 	OpConstant Opcode = iota
+	ok
 )
 
 type Definition struct {
@@ -31,6 +70,7 @@ func Lookup(op byte) (*Definition, error) {
 	return def, nil
 }
 
+// Make converts an opcode and operands into bytecode.
 func Make(op Opcode, operands ...int) []byte {
 	def, ok := definitions[op]
 	if !ok {
@@ -56,4 +96,28 @@ func Make(op Opcode, operands ...int) []byte {
 	}
 
 	return instruction
+}
+
+// ReadOperands is the inverse of Make - it converts operands from bytecode into
+// an integer. For now, it only supports integer - I assume we'll support more
+// types later. Returns the decoded operands and how many bytes it read to
+// decode the operands.
+func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
+	operands := make([]int, len(def.OperandWidths))
+	offset := 0
+
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(ins[offset:]))
+		}
+
+		offset += width
+	}
+
+	return operands, offset
+}
+
+func ReadUint16(ins Instructions) uint16 {
+	return binary.BigEndian.Uint16(ins)
 }
---
+++ writing-a-compiler-in-go/src/monkey/compiler/compiler.go
@@ -0,0 +1,40 @@
+package compiler
+
+import (
+	"monkey/ast"
+	"monkey/code"
+	"monkey/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+
+	// Constants used to that we can refer to constant values directly
+	// without needing to pop them on/off the stack.
+	//
+	// We use the `object.Object` type we used with the ast.
+	Constants []object.Object
+}
