ec1c279cccbb60bd131f0055346ac0e3099042c9
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-08-07 08:34:23 -0400

    [WIP] Chapter 2
    Compiler is generating first instructions! We can now emit the
    instructions for adding two constants.
    
    The VM has a concept of constants, which is values it can lookup
    directly without having to load them onto the stack.

--- writing-a-compiler-in-go/src/monkey/compiler/compiler.go
+++ writing-a-compiler-in-go/src/monkey/compiler/compiler.go
@@ -18,8 +18,63 @@ func New() *Compiler {
 	}
 }
 
+// Compile updates the internal `instructions` array with the bytecode
+// representation of the instructions.
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+
 	return nil
+}
+
+// addConstants adds a constant into the constant pool, and then returns the
+// index of the constant in the pool (which can be passed as the argument to an
+// `OpConstant` instruction).
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+// emit generates bytecode and saves it to the compilers in memory list of
+// instructions. It returns the starting point of the just emitted instructions.
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstruction(ins)
+	return pos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
 }
 
 func (c *Compiler) Bytecode() *Bytecode {
