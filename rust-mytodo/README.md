b7a4da6f69d490087caef8b76cc5177188461186
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-09-05 10:22:49 -0400

    Work through Rust Web app demo

---
+++ rust-mytodo/README.md
@@ -0,0 +1,12 @@
+# Rust Web App
+
+https://erwabook.com
+
+Working through for fun.
+
+Ensure we install `libpq-dev`.
+
+Run `cargo install diesel_cli --no-default-features --features postgres`
+
+Use `docker run -e POSTGRES_USER=username -e POSTGRES_PASSWORD=password -e
+POSTGRES_DB=rust-mytodo -p 5434:5432 -d postgres:10`
---
+++ rust-mytodo/src/db/mod.rs
@@ -0,0 +1,25 @@
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+
+pub mod models;
+pub mod schema;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn create_task<'a>(connection: &PgConnection, title: &'a str) {
+    let task = models::NewTask { title };
+
+    diesel::insert_into(schema::task::table)
+        .values(&task)
+        .execute(connection)
+        .expect("Error inserting new task");
+}
