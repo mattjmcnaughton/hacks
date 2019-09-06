9d6d36a1513caa195aef548416221f1f96f81876
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-09-05 21:38:17 -0400

    Add basic rest endpoint via Rocket

---
+++ rust-mytodo/src/bin/backend.rs
@@ -0,0 +1,24 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use mytodo::db::{query_task, establish_connection};
+
+#[get("/tasks")]
+fn tasks_get() -> String {
+    let mut response: Vec<String> = vec![];
+
+    let conn = establish_connection();
+    for task in query_task(&conn) {
+        response.push(task.title);
+    }
+
+    response.join("\n")
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![tasks_get])
+        .launch();
+}
