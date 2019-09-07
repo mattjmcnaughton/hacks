2ac2c3b49d6cd8de95cca7fbb4dbbdcd989a743c
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-09-07 13:17:26 -0400

    Add simple wasm example
    Create a simple wasm front end that will display.
    
    Note, we need to run this behind a server or there will be CORs issues.
    Use `python3 -m http.server` from the `frontend` dir.

--- rust-mytodo/frontend/src/lib.rs
+++ rust-mytodo/frontend/src/lib.rs
@@ -1,7 +1,55 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use]
+extern crate seed;
+use seed::prelude::*;
+
+#[derive(Clone, Debug)]
+enum Direction {
+    Coming,
+    Going,
+}
+
+struct Model {
+    direction: Direction,
+}
+
+#[derive(Clone, Debug)]
+enum Msg {
+    Click,
+}
+
+fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Click => {
+            model.direction = match model.direction {
+                Direction::Coming => Direction::Going,
+                Direction::Going => Direction::Coming,
+            }
+        }
     }
+}
+
+fn view(model: &Model) -> impl View<Msg> {
+    let greeting = match model.direction {
+        Direction::Coming => "Hello, World!",
+        Direction::Going => "Hasta la vista!",
+    };
+    h1![
+        class! {"heading"},
+        style!["height" => "100vh",
+               "width" => "100vw",
+        ],
+        { greeting },
+        simple_ev(Ev::Click, Msg::Click),
+    ]
+}
+
+fn init(_url: Url, _orders: &mut impl Orders<Msg>) -> Model {
+    Model {
+        direction: Direction::Coming,
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn render() {
+    seed::App::build(init, update, view).finish().run();
 }
