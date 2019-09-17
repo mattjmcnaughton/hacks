46dd1942e6809082b4fad1ed2a017d7501fe09b9
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-09-17 10:05:20 -0400

    [WACIG Chapter 4] Support executing jumps
    Our compiler now supports executing jumps. The final step is handling
    the `null` the results from an expression like `if ( false ) { 10; }`.

--- writing-a-compiler-in-go/src/monkey/vm/vm.go
+++ writing-a-compiler-in-go/src/monkey/vm/vm.go
@@ -92,6 +92,19 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+
+		case code.OpJump:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip = pos - 1
+
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+
+			condition := vm.pop()
+			if !isTruthy(condition) {
+				ip = pos - 1
+			}
 		}
 
 	}
@@ -202,6 +215,16 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 		return True
 	}
 	return False
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		return obj.Value
+
+	default:
+		return true
+	}
 }
 
 func (vm *VM) executeBangOperator() error {
