de43c6f8b8c3211129b1e28f8beed11d2d789d85
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-09-28 20:20:08 -0400

    (WACIG Chapter 6) Support array construction in VM
    Add support for executing the array constructor in the VM. Adding
    support for hashes is next.

--- writing-a-compiler-in-go/src/monkey/vm/vm.go
+++ writing-a-compiler-in-go/src/monkey/vm/vm.go
@@ -134,10 +134,32 @@ func (vm *VM) Run() error {
 			ip += 2
 
 			vm.push(vm.globals[globalIndex])
+
+		case code.OpArray:
+			numElements := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+
+			array := vm.buildArray(vm.sp-numElements, vm.sp)
+			vm.sp = vm.sp - numElements
+
+			err := vm.push(array)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
 	return nil
+}
+
+func (vm *VM) buildArray(startIndex, endIndex int) object.Object {
+	elements := make([]object.Object, endIndex-startIndex)
+
+	for i := startIndex; i < endIndex; i++ {
+		elements[i-startIndex] = vm.stack[i]
+	}
+
+	return &object.Array{Elements: elements}
 }
 
 func (vm *VM) push(o object.Object) error {
