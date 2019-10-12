eff598b9f0c2048121516f5b1cfb17c4b4ec2a78
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-10-12 12:13:36 -0400

    [tlpi] Do `umask` example
    Write an example C programming using `umask`.

--- tlpi-dist/files/t_umask.c
+++ tlpi-dist/files/t_umask.c
@@ -20,9 +20,12 @@
 #include "tlpi_hdr.h"
 
 #define MYFILE "myfile"
-#define MYDIR  "mydir"
+#define MYDIR "mydir"
+// Ask for rw-rw----
 #define FILE_PERMS    (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
+// Ask for rwxrwxrwx
 #define DIR_PERMS     (S_IRWXU | S_IRWXG | S_IRWXO)
+// By default, group and other can not write or execute (i.e. deny ----wx-wx)
 #define UMASK_SETTING (S_IWGRP | S_IXGRP | S_IWOTH | S_IXOTH)
 
 int
@@ -32,30 +35,37 @@ main(int argc, char *argv[])
     struct stat sb;
     mode_t u;
 
+    // System call to set the umask for the process.
     umask(UMASK_SETTING);
 
+    // Open file for read/write, create if doesn't exist, and fail if pathname
+    // already exists.
     fd = open(MYFILE, O_RDWR | O_CREAT | O_EXCL, FILE_PERMS);
     if (fd == -1)
         errExit("open-%s", MYFILE);
     if (mkdir(MYDIR, DIR_PERMS) == -1)
         errExit("mkdir-%s", MYDIR);
 
-    u = umask(0);               /* Retrieves (and clears) umask value */
+    // Retrieve and clear the umask.
+    u = umask(0);
 
     if (stat(MYFILE, &sb) == -1)
         errExit("stat-%s", MYFILE);
     printf("Requested file perms: %s\n", filePermStr(FILE_PERMS, 0));
-    printf("Process umask:        %s\n", filePermStr(u, 0));
-    printf("Actual file perms:    %s\n\n", filePermStr(sb.st_mode, 0));
+    printf("Process umask: %s\n", filePermStr(u, 0));
+    // `sb.st_mode` gives the permissions for the file.
+    printf("Actual file perms: %s\n", filePermStr(sb.st_mode, 0));
 
     if (stat(MYDIR, &sb) == -1)
         errExit("stat-%s", MYDIR);
-    printf("Requested dir. perms: %s\n", filePermStr(DIR_PERMS, 0));
-    printf("Process umask:        %s\n", filePermStr(u, 0));
-    printf("Actual dir. perms:    %s\n", filePermStr(sb.st_mode, 0));
+    printf("Requested dir perms: %s\n", filePermStr(DIR_PERMS, 0));
+    printf("Process umask: %s\n", filePermStr(u, 0));
+    // `sb.st_mode` gives the permissions for the dir.
+    printf("Actual dir perms: %s\n", filePermStr(sb.st_mode, 0));
 
+    // Delete file
     if (unlink(MYFILE) == -1)
-        errMsg("unlink-%s", MYFILE);
+        errMsg("unline-%s", MYFILE);
     if (rmdir(MYDIR) == -1)
         errMsg("rmdir-%s", MYDIR);
     exit(EXIT_SUCCESS);
