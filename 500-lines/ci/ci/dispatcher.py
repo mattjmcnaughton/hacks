c25b76b77dab6bbf0a0b36668f96868f76dd706e
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-12-26 20:02:13 -0500

    [WIP][500-lines] Continuing working on CI example
    Dispatcher is in place, although possible its not working entirely as
    expected.
    
    Next steps:
    - Add test_runner.py.
    - Add e2e test.
    - Start refactoring via legacy code methods.

--- 500-lines/ci/ci/dispatcher.py
+++ 500-lines/ci/ci/dispatcher.py
@@ -8,15 +8,102 @@ It can register as many test runners as you like.
 """
 
 import argparse
-import SocketServer
+import re
+import os
+import socket
+import socketserver
+import time
+import threading
 
-class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    pass
-    # TODO
+import helpers
 
-class DispatcherHandler(SocketServer.BaseRequestHandler):
-    pass
-    # TODO
+def dispatch_tests(server, commit_id):
+    # In practice, this method will normally not run forever.
+    while True:
+        print("Trying to dispatch to runners")
+        for runner in server.runners:
+            response = helpers.communicate(runner["host"],
+                    int(runner["port"]), "runtest:%s" % commit_id)
+
+            if response == "OK":
+                print("adding id %s" % commit_id)
+                server.dispatched_commits[commit_id] = runner
+                if commit_id in server.pending_commits:
+                    server.pending_commits.remove(commit_id)
+                return
+
+        time.sleep(2)
+
+class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    runners = [] # Keeps track of test runner pool
+    dead = False # Indicate to other threads that we are no longer running
+    dispatched_commits = {} # Keeps track of commits we've dispatched
+    pending_commits = [] # Keep track of commits that we've yet to dispatch
+
+
+class DispatcherHandler(socketserver.BaseRequestHandler):
+    """
+    The RequestHandler class for our dispatcher. This will dispatch test runners
+    against the incoming commit and handle their requests and test results.
+
+    https://docs.python.org/3.7/library/socketserver.html#socketserver.BaseRequestHandler
+    """
+    command_re = re.compile(r"(\w+)(:.+)*")
+    BUF_SIZE = 1024
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client.
+        self.data = self.request.recv(self.BUF_SIZE).strip().decode()
+        command_groups = self.command_re.match(self.data)
+
+        if not command_groups:
+            self.request.sendall(b"Invalid command")
+            return
+        command = command_groups.group(1)
+
+        if command == "status":
+            print("in status")
+            self.request.sendall(b"OK")
+
+        elif command == "register":
+            # Add the test runner to the pool
+            print("register")
+            address = command_groups.group(2)
+            host, port = re.findall(r":(\w*)", address)
+            runner = {"host": host, "port": int(port)}
+            self.server.runners.append(runner)
+            self.request.sendall(b"OK")
+
+        elif command == "dispatch":
+            print("going to dispatch")
+            commit_id = command_groups.group(2)[1:]
+            if not self.server.runners:
+                self.request.sendall(b"No runners are registered")
+            else:
+                self.request.sendall(b"OK")
+                dispatch_tests(self.server, commit_id)
+
+        elif command == "results":
+            print("got test results")
+            results = command_groups.group(2)[1:]
+            results = results.split(":")
+            commit_id = results[0]
+            length_msg = int(results[1])
+            # 3 is the bumber of ":" in the sent command
+            remaining_buffer = self.BUF_SIZE - (len(command) + len(commit_id) + len(results[1]) + 3)
+            if length_msg > remaining_buffer:
+                self.data += self.request.recv(length_msg - remaining_buffer).strip()
+            del self.server.dispatched_commits[commit_id]
+            if not os.path.exists("test_results"):
+                os.makedirs("test_results")
+            with open("test_results/%s" % commit_id, "w") as f:
+                data = self.data.split(":")[3:]
+                data = "\n".join(data)
+                f.write(data)
+            self.request.sendall(b"OK")
+        else:
+            self.request.sendall(b"Invalid command")
+
 
 def serve():
     parser = argparse.ArgumentParser()
@@ -32,8 +119,55 @@ def serve():
 
     # Create the server
     server = ThreadingTCPServer((args.host, int(args.port)), DispatcherHandler)
+    print("serving on %s:%s" % (args.host, int(args.port)))
 
-    # TODO
+    # Create a thread to check the runner pool
+    def runner_checker(server):
+        # Manages the commit lists when a runner fails
+        def manage_commit_lists(runner):
+            for commit, assigned_runner in server.dispatched_commits.items():
+                if assigned_runner == runner:
+                    del server.dispatched_commits[commit]
+                    server.pending_commits.append(commit)
+                    break
+            server.runners.remove(runner)
+
+        while not server.dead:
+            time.sleep(1)
+            for runner in server.runners:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                try:
+                    response = helpers.communicate(runner["host"],
+                            int(runner["port"]), "ping")
+                    if response != "pong":
+                        print("Removing runner %s" % runner)
+                        manage_commit_lists(runner)
+                except socket.error as e:
+                    manage_commit_lists(runner)
+
+    # Create a thread to kick off tests that failed
+    def redistribute(server):
+        while not server.dead:
+            for commit in server.pending_commits:
+                print("running redistribute")
+                print(server.pending_commits)
+                dispatch_tests(server, commit)
+                time.sleep(5)
+
+    runner_heartbeat = threading.Thread(target=runner_checker, args=(server,))
+    redistributor = threading.Thread(target=redistribute, args=(server,))
+
+    try:
+        runner_heartbeat.start()
+        redistributor.start()
+
+        # Activate the server; it'll run until the program is interrupted.
+        server.serve_forever()
+    except (KeyboardInterrupt, Exception):
+        # If any exception, kill all threads.
+        server.dead = True
+        runner_heartbeat.join()
+        redistributor.join()
 
 
 if __name__ == "__main__":
--- 500-lines/ci/ci/helpers.py
+++ 500-lines/ci/ci/helpers.py
@@ -3,7 +3,7 @@ import socket
 def communicate(host, port, request):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
-    s.send(request)
+    s.send(request.encode())
     response = s.recv(1024)
     s.close()
     return response
