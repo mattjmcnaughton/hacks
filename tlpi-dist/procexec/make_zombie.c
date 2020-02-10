0a528884af928ff6d2b122e088461ec0b5e9de78
mattjmcnaughton <mattjmcnaughton@gmail.com>
2020-02-10 09:24:23 -0500

    [tlpi] Explore zombie processes
    Program interacting w/ zombie processes.

--- tlpi-dist/procexec/make_zombie.c
+++ tlpi-dist/procexec/make_zombie.c
@@ -29,31 +29,28 @@ main(int argc, char *argv[])
     char cmd[CMD_SIZE];
     pid_t childPid;
 
-    setbuf(stdout, NULL);       /* Disable buffering of stdout */
-
+    setbuf(stdout, NULL); /* disable buffering stdout */
     printf("Parent PID=%ld\n", (long) getpid());
 
     switch (childPid = fork()) {
     case -1:
         errExit("fork");
 
-    case 0:     /* Child: immediately exits to become zombie */
+    case 0: /* Child: immediately exits to become zombie */
         printf("Child (PID=%ld) exiting\n", (long) getpid());
         _exit(EXIT_SUCCESS);
 
-    default:    /* Parent */
-        sleep(3);               /* Give child a chance to start and exit */
+    default: /* Parent */
+        sleep(3);
         snprintf(cmd, CMD_SIZE, "ps | grep %s", basename(argv[0]));
-        system(cmd);            /* View zombie child */
+        system(cmd); /* View zombie child */
 
         /* Now send the "sure kill" signal to the zombie */
-
         if (kill(childPid, SIGKILL) == -1)
             errMsg("kill");
-        sleep(3);               /* Give child a chance to react to signal */
+        sleep(3); /* Give child chance to react... although nothing will happen bc zombie */
         printf("After sending SIGKILL to zombie (PID=%ld):\n", (long) childPid);
-        system(cmd);            /* View zombie child again */
-
+        system(cmd); /* View zombie child again */
         exit(EXIT_SUCCESS);
     }
 }
