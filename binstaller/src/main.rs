e16092b0582b5e2e68235d45a3041aaf356e71f1
mattjmcnaughton <mattjmcnaughton@gmail.com>
2021-08-29 16:47:18 -0400

    binstaller: Start dividing into functions
    Some tricky box/dyn stuff around errors, but overall started the process
    of breaking up into functions.

--- binstaller/src/main.rs
+++ binstaller/src/main.rs
@@ -1,4 +1,6 @@
+use std::error::Error;
 use std::fs;
+use std::io::Read;
 use std::path::Path;
 
 use quicli::prelude::*;
@@ -17,18 +19,23 @@ struct Cli {
     verbosity: Verbosity,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::from_args();
     args.verbosity.setup_env_logger("binstaller")?;
 
-    let url = reqwest::Url::parse(&args.remote_url)?;
+    run(&args.remote_url, &args.destination_directory, &args.binary_name)
+}
 
-    // Nested use of `ok_or` seems pretty hacky, but necessary for `Result`.
-    let artifact_name = url.path_segments().ok_or("Cannot parse url")?.last().ok_or("Cannot parse url")?;
+fn run(remote_url: &String, destination_directory: &String, binary_name: &String) -> Result<(), Box<dyn Error>> {
+    let url = reqwest::Url::parse(remote_url)?;
+
+    let artifact_path_segments = url.path_segments().ok_or(format!("Cannot parse {} into path segments", remote_url))?;
+    let artifact_name = artifact_path_segments.last().ok_or(format!("Cannot extract artifact name from path segments returned by {}", remote_url))?;
 
     // Long-term, long into async w/ reqwest.
     let res = reqwest::blocking::get(url.as_str())?;
-    let mut archive = Archive::new(XzDecoder::new(res));
+    let decoder = decoder_factory(artifact_name, res)?;
+    let mut archive = Archive::new(decoder);
 
     let tmp_dir = tempdir()?;
     archive.unpack(tmp_dir.path())?;
@@ -38,10 +45,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Definitely need a better solution here (i.e. we can't just assume its a tar)
     let extracted_artifact_name = artifact_name.split(".tar").nth(0).ok_or("Cannot extract directory from compressed file name")?;
 
-    let src_path = tmp_dir.path().join(extracted_artifact_name).join(&args.binary_name);
-    let dest_path = Path::new(&args.destination_directory).join(&args.binary_name);
+    let src_path = tmp_dir.path().join(extracted_artifact_name).join(binary_name);
+    let dest_path = Path::new(destination_directory).join(binary_name);
 
     fs::copy(src_path, dest_path)?;
 
     Ok(())
+}
+
+fn decoder_factory(remote_artifact_name: &str, result: reqwest::blocking::Response) -> Result<Box<dyn Read>, Box<dyn Error>> {
+    if remote_artifact_name.ends_with(".xz") {
+        Ok(Box::new(XzDecoder::new(result)))
+    } else {
+        // https://doc.rust-lang.org/std/convert/trait.From.html#impl-From%3C%26%27_%20str%3E
+        let boxed_err = Box::<dyn Error>::from(format!("Cannot determine decoder for {}", remote_artifact_name));
+        Err(boxed_err)
+    }
 }
