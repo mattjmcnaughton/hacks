43e7ae4513719f76a5063a9e43ba3dc8e24f9f72
mattjmcnaughton <mattjmcnaughton@gmail.com>
2021-08-28 22:57:33 -0400

    Add cli-options for binstaller
    `quicli` seems like a popular project. Create separate project for
    `binstaller` soon.

--- binstaller/src/main.rs
+++ binstaller/src/main.rs
@@ -1,16 +1,27 @@
 use std::fs;
 use std::path::Path;
 
+use quicli::prelude::*;
+use structopt::StructOpt;
+
 use tar::Archive;
 use tempfile::tempdir;
 use xz2::read::XzDecoder;
 
-const REMOTE_URL: &str  = "https://github.com/ziglang/zig/releases/download/0.7.1/zig-linux-x86_64-0.7.1.tar.xz";
-const BINARY_DIRECTORY: &str = "/tmp";
-const BINARY_NAME: &str = "zig";
+#[derive(Debug, StructOpt)]
+struct Cli {
+    remote_url: String,
+    destination_directory: String,
+    binary_name: String,
+    #[structopt(flatten)]
+    verbosity: Verbosity,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = reqwest::Url::parse(REMOTE_URL)?;
+    let args = Cli::from_args();
+    args.verbosity.setup_env_logger("binstaller")?;
+
+    let url = reqwest::Url::parse(&args.remote_url)?;
 
     // Nested use of `ok_or` seems pretty hacky, but necessary for `Result`.
     let artifact_name = url.path_segments().ok_or("Cannot parse url")?.last().ok_or("Cannot parse url")?;
@@ -22,11 +33,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let tmp_dir = tempdir()?;
     archive.unpack(tmp_dir.path())?;
 
+    info!("Unpacking tar into {:?}", tmp_dir.path());
+
     // Definitely need a better solution here (i.e. we can't just assume its a tar)
     let extracted_artifact_name = artifact_name.split(".tar").nth(0).ok_or("Cannot extract directory from compressed file name")?;
 
-    let src_path = tmp_dir.path().join(extracted_artifact_name).join(BINARY_NAME);
-    let dest_path = Path::new(BINARY_DIRECTORY).join(BINARY_NAME);
+    let src_path = tmp_dir.path().join(extracted_artifact_name).join(&args.binary_name);
+    let dest_path = Path::new(&args.destination_directory).join(&args.binary_name);
 
     fs::copy(src_path, dest_path)?;
 
