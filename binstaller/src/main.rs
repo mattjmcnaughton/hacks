7a5aea14aa8dc6214680ed2ea1083f5908e7b005
mattjmcnaughton <mattjmcnaughton@gmail.com>
2021-08-30 10:42:33 -0400

    binstaller: Add support for .gz
    Close to the time to start breaking this up into more modular
    components.

--- binstaller/src/main.rs
+++ binstaller/src/main.rs
@@ -3,9 +3,9 @@ use std::fs;
 use std::io::Read;
 use std::path::Path;
 
+use flate2::read::GzDecoder;
 use quicli::prelude::*;
 use structopt::StructOpt;
-
 use tar::Archive;
 use tempfile::tempdir;
 use xz2::read::XzDecoder;
@@ -14,6 +14,7 @@ use xz2::read::XzDecoder;
 struct Cli {
     remote_url: String,
     destination_directory: String,
+    extracted_artifact_base: String,
     binary_name: String,
     #[structopt(flatten)]
     verbosity: Verbosity,
@@ -23,10 +24,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::from_args();
     args.verbosity.setup_env_logger("binstaller")?;
 
-    run(&args.remote_url, &args.destination_directory, &args.binary_name)
+    run(&args.remote_url, &args.destination_directory, &args.extracted_artifact_base, &args.binary_name)
 }
 
-fn run(remote_url: &String, destination_directory: &String, binary_name: &String) -> Result<(), Box<dyn Error>> {
+fn run(remote_url: &String, destination_directory: &String, extracted_artifact_base: &String, binary_name: &String) -> Result<(), Box<dyn Error>> {
     let url = reqwest::Url::parse(remote_url)?;
 
     let artifact_path_segments = url.path_segments().ok_or(format!("Cannot parse {} into path segments", remote_url))?;
@@ -38,16 +39,16 @@ fn run(remote_url: &String, destination_directory: &String, binary_name: &String
     let mut archive = Archive::new(decoder);
 
     let tmp_dir = tempdir()?;
-    archive.unpack(tmp_dir.path())?;
 
     info!("Unpacking tar into {:?}", tmp_dir.path());
+    archive.unpack(tmp_dir.path())?;
 
-    // Definitely need a better solution here (i.e. we can't just assume its a tar)
-    let extracted_artifact_name = artifact_name.split(".tar").nth(0).ok_or("Cannot extract directory from compressed file name")?;
+    let src_path = tmp_dir.path().join(extracted_artifact_base).join(binary_name);
 
-    let src_path = tmp_dir.path().join(extracted_artifact_name).join(binary_name);
-    let dest_path = Path::new(destination_directory).join(binary_name);
+    // TODO: This is hacky
+    let dest_path = Path::new(destination_directory).join(Path::new(binary_name).file_name().unwrap());
 
+    info!("Copying file from {:?} to {:?}", src_path, dest_path);
     fs::copy(src_path, dest_path)?;
 
     Ok(())
@@ -55,7 +56,11 @@ fn run(remote_url: &String, destination_directory: &String, binary_name: &String
 
 fn decoder_factory(remote_artifact_name: &str, result: reqwest::blocking::Response) -> Result<Box<dyn Read>, Box<dyn Error>> {
     if remote_artifact_name.ends_with(".xz") {
+        info!("Identified extractable file type: {}", "xz");
         Ok(Box::new(XzDecoder::new(result)))
+    } else if remote_artifact_name.ends_with(".gz") {
+        info!("Identified extractable file type: {}", "gz");
+        Ok(Box::new(GzDecoder::new(result)))
     } else {
         // https://doc.rust-lang.org/std/convert/trait.From.html#impl-From%3C%26%27_%20str%3E
         let boxed_err = Box::<dyn Error>::from(format!("Cannot determine decoder for {}", remote_artifact_name));
