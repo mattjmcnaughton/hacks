6a87367a2037831e444af959172b38b370476a59
mattjmcnaughton <mattjmcnaughton@gmail.com>
2021-10-06 09:47:58 -0400

    [WIP] Experiment w/ vue3 and typescript
    Following https://www.bacancytechnology.com/blog/vue3-typescript.

---
+++ vue-playground/vue-todo-app/src/views/AddTask/AddTask.ts
@@ -0,0 +1,15 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "AddTask",
+    data() {
+        return {
+            taskName: ""
+        };
+    },
+    methods: {
+        addTask(): void {
+
+        }
+    }
+});
